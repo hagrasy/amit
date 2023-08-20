dbb26c870419b0355d0857621213a11a847e615d
hagrasy <134404345+hagrasy@users.noreply.github.com>
2023-08-20 23:15:18 +0300

    onboarding screen

--- lib/screens/onboarding.dart
+++ lib/screens/onboarding.dart
@@ -1,29 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/model/onboarding_model.dart';
+import 'package:note_app/widgets/default_text.dart';
 
-class OnBoarding extends StatelessWidget {
+import '../views/item_onboarding.dart';
+
+class OnBoarding extends StatefulWidget {
   const OnBoarding({super.key});
 
   @override
+  State<OnBoarding> createState() => _OnBoardingState();
+}
+
+class _OnBoardingState extends State<OnBoarding> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          actions: [
+            TextButton(
+                onPressed: () {},
+                child: DefaultText(
+                  text: "Skip",
+                  color: Colors.teal,
+                  fontSize: 15,
+                ))
+          ],
+        ),
         body: SizedBox(
-            height: double.infinity,
-            width: double.infinity,
-            child: SafeArea(
-                child: Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 20),
-                    child: Form(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                          Text(
-                            "OnBoarding",
-                            style: TextStyle(
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.orange,
-                            ),
-                          ),
-                        ]))))));
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: PageView.builder(
+                  itemCount: 2,
+                  itemBuilder: (context, index) =>
+                      //                   OnBoardingItem(onBoardingModel: itemOnBoard[index])),
+                      //         ),
+                      //       ],
+                      //     ),
+                      //   ),
+                      // );
+
+                      Image.asset(OnBoardingModel!.image),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ))
+              ],
+            )));
   }
 }
